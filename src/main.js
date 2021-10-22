cb978b2d5a7bcd93a5be45bbfdb0e82f5e8c9637
Aleksander <65820898+rayydev@users.noreply.github.com>
2021-10-22 16:36:29 +0200

    added files

---
+++ src/main.js
@@ -0,0 +1,18 @@
+let sendBtn = document.getElementById('sendBtn');
+let messageTb = document.getElementById('messageTb');
+let Content = document.getElementById('content');
+
+//console info for no reason lolz
+console.warn("rayydev Fakecord \n ", "If you on this console you are hacker \n plz dont hack ");
+
+//send action
+
+/*sendBtn.addEventListener('click', function () {
+    var messagetext = document.createElement('txt');
+    messagetext.innerText = messageTb.value;
+    Content.appendChild(messagetext);
+    messageTb.value = "";
+
+})*/
+
+
